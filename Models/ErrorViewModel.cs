021db4a06888b6899c4382e23eb44e714ec410d3
edySnake <contactoedysnake@gmail.com>
2023-04-16 07:03:25 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetPathWepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
